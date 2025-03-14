fa460b9b57c0aae5b7f2b487f03a4deac06a36d1
kgarri <black.hat.kg@gmail.com>
2025-03-13 21:15:07 -0500

    changed the stup to give living players esp

--- AdminESP.cs
+++ AdminESP.cs
@@ -37,6 +37,27 @@ public sealed partial class AdminESP : BasePlugin, IPluginConfig<Config>
         }
 
     }
+    
+    private CCSPlayerController GetPlayerFromUserName(String playerName)
+    {
+
+        List<CCSPlayerController> players = Utilities.GetPlayers();
+
+        foreach (CCSPlayerController player in players)
+        {
+            Console.WriteLine(player.PlayerName);
+            Console.WriteLine(player.Slot);
+            Console.WriteLine(player.SteamID);
+            if (string.Equals(player.PlayerName, playerName, StringComparison.CurrentCultureIgnoreCase))
+            {
+                return player;
+            }
+        }
+
+        throw new Exception($"Player {playerName} not found");
+        
+    }
+
 
     public override void Unload(bool hotReload)
     {
@@ -45,32 +66,24 @@ public sealed partial class AdminESP : BasePlugin, IPluginConfig<Config>
     }
 
     [ConsoleCommand("css_esp", "Toggle Admin ESP")]
-    [CommandHelper(minArgs: 0, whoCanExecute: CommandUsage.CLIENT_ONLY)]
-    public void OnToggleAdminEsp(CCSPlayerController? player, CommandInfo command)
+    [CommandHelper(minArgs: 1,"[name]", whoCanExecute: CommandUsage.CLIENT_ONLY)]
+    public void OnToggleAdminEsp(CCSPlayerController? adminPlayer, CommandInfo command)
     {
+        String playerName = command.ArgByIndex(1);
+        CCSPlayerController player = GetPlayerFromUserName(playerName);
 
         if (player is null || player.IsValid is not true) return;
 
-        if (AdminManager.PlayerHasPermissions(player, Config.AdminFlag) is not true) {
+        if (AdminManager.PlayerHasPermissions(adminPlayer, Config.AdminFlag) is not true) {
 
-            SendMessageToSpecificChat(player, msg: "Admin ESP can only be used from {GREEN}admins{DEFAULT}!", print: PrintTo.Chat);
+            SendMessageToSpecificChat(adminPlayer, msg: "Admin ESP can only be used from {GREEN}admins{DEFAULT}!", print: PrintTo.Chat);
             return;  
         }
 
         switch (player.PawnIsAlive) {
             
             case true:
-
-                if (Config.AllowDeadAdminESP is true) {
-                    SendMessageToSpecificChat(player, msg: "You should be {RED}dead {DEFAULT}to use Admin ESP!", print: PrintTo.Chat);
-                    return;
-                }
-                SendMessageToSpecificChat(player, msg: "Admin ESP is only allowed while {RED}spectating{DEFAULT}!", print: PrintTo.Chat);
-
-            break;
-            case false:
-
-                if (player.Team is CsTeam.Spectator) {
+                
                     toggleAdminESP[player.Slot] = !toggleAdminESP[player.Slot];
 
                     if (toggleAdminESP[player.Slot] is true) {
@@ -91,34 +104,16 @@ public sealed partial class AdminESP : BasePlugin, IPluginConfig<Config>
 
                     SendMessageToSpecificChat(player, msg: $"Admin ESP has been " + (toggleAdminESP[player.Slot] ? "{GREEN}enabled!" : "{RED}disabled!"), print: PrintTo.Chat); 
                     return;
+            
+            case false:
+                if (Config.AllowDeadAdminESP is true) {
+                    SendMessageToSpecificChat(player, msg: "You should be {RED}dead {DEFAULT}to use Admin ESP!", print: PrintTo.Chat);
+                    return;
                 }
-                else {
-                    if (Config.AllowDeadAdminESP is true) {
-                        toggleAdminESP[player.Slot] = !toggleAdminESP[player.Slot];
+                SendMessageToSpecificChat(player, msg: "Admin ESP is only allowed while {RED}spectating{DEFAULT}!", print: PrintTo.Chat);
 
-                        if (toggleAdminESP[player.Slot] is true) {
-
-                            if (togglePlayersGlowing is not true || AreThereEsperingAdmins() is not true) {
-                                SetAllPlayersGlowing();
-                            }
-                            //player.ReplicateConVar("mp_forcecamera", "0");
-                        }
-                        else {
-
-                            if (togglePlayersGlowing is not true || AreThereEsperingAdmins() is not true) {
-                                RemoveAllGlowingPlayers();
-                            }
-                            //player.ReplicateConVar("mp_forcecamera", _forceCamera is not null ? _forceCamera.GetPrimitiveValue<int>().ToString() : "1");
-                        }
-
-                        SendMessageToSpecificChat(player, msg: $"Admin ESP has been " + (toggleAdminESP[player.Slot] ? "{GREEN}enabled!" : "{RED}disabled!"), print: PrintTo.Chat);
-                        return;
-                    
-                    }
-                }
-
-                SendMessageToSpecificChat(player, msg: "Admin ESP is only allowed in {RED}spectate mode{DEFAULT}!", print: PrintTo.Chat);
-            break;
+                break;
+               
         
 
         }
--- Core/Config.cs
+++ Core/Config.cs
@@ -8,7 +8,7 @@ public class Config : BasePluginConfig
     [JsonPropertyName("debug_mode")] public bool DebugMode { get; set; } = true;
     [JsonPropertyName("chat_prefix")] public string ChatPrefix { get; set; } = "{GREEN}[AdminESP]";
     [JsonPropertyName("admin_flag")] public string AdminFlag { get; set; } = "@css/ban";
-    [JsonPropertyName("hide_from_spectator_list")] public bool HideAdminSpectators { get; set; } = true;
+    [JsonPropertyName("hide_from_spectator_list")] public bool HideAdminSpectators { get; set; } = false;
     [JsonPropertyName("allow_esp_when_dead")] public bool AllowDeadAdminESP { get; set; } = true;
     [JsonPropertyName("skip_spectating_espers")] public bool SkipSpectatingEsps { get; set; } = true;
 
